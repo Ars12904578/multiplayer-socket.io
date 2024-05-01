9afecd5647b325206be5bad652a1244a11341e1c
Ars1290 <90035929+Ars12904578@users.noreply.github.com>
2024-05-01 06:11:24 +0000

    change port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
