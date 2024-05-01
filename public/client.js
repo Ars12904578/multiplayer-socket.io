4855ded55a7ebf5ba959623295c6a1dd042522b8
Ars1290 <90035929+Ars12904578@users.noreply.github.com>
2024-05-01 11:05:30 +0000

    update

--- public/client.js
+++ public/client.js
@@ -20,69 +20,49 @@ item.textContent = data;messages.appendChild(item);
 window.scrollTo(0, document.body.scrollHeight);});
 window.addEventListener('beforeunload', function() {
 socket.emit('disconnect');});function makeDraggable(element) {
-    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    element.addEventListener('mousedown', dragMouseDown);
-    element.addEventListener('touchstart', dragMouseDown);
-    function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-        pos3 = e.type === 'mousedown' ? e.clientX : e.touches[0].clientX;
-        pos4 = e.type === 'mousedown' ? e.clientY : e.touches[0].clientY;
-        document.addEventListener('mouseup', closeDragElement);
-        document.addEventListener('touchend', closeDragElement);
-        document.addEventListener('touchmove', elementDrag);
-        document.addEventListener('mousemove', elementDrag);
-    }function elementDrag(e) {
-        e = e || window.event;
-        pos1 = pos3 - (e.type === 'mousemove' ? e.clientX : e.touches[0].clientX);
-        pos2 = pos4 - (e.type === 'mousemove' ? e.clientY : e.touches[0].clientY);
-        pos3 = e.type === 'mousemove' ? e.clientX : e.touches[0].clientX;
-        pos4 = e.type === 'mousemove' ? e.clientY : e.touches[0].clientY;
-        element.style.top = (element.offsetTop - pos2) + "px";
-        element.style.left = (element.offsetLeft - pos1) + "px";
-        // Send the element's position to the server
-        socket.emit('position', { id: element.id, x: element.offsetLeft, y: element.offsetTop });
-    }function closeDragElement() {
-        document.removeEventListener('mouseup', closeDragElement);
-        document.removeEventListener('mousemove', elementDrag);
-        document.removeEventListener('touchend', closeDragElement);
-        document.removeEventListener('touchmove', elementDrag);
-    }}
+let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+element.addEventListener('mousedown', dragMouseDown);
+element.addEventListener('touchstart', dragMouseDown);
+function dragMouseDown(e) {e = e || window.event;e.preventDefault();
+pos3 = e.type === 'mousedown' ? e.clientX : e.touches[0].clientX;
+pos4 = e.type === 'mousedown' ? e.clientY : e.touches[0].clientY;
+document.addEventListener('mouseup', closeDragElement);
+document.addEventListener('touchend', closeDragElement);
+document.addEventListener('touchmove', elementDrag);
+document.addEventListener('mousemove', elementDrag);
+}function elementDrag(e) {e = e || window.event;
+pos1 = pos3 - (e.type === 'mousemove' ? e.clientX : e.touches[0].clientX);
+pos2 = pos4 - (e.type === 'mousemove' ? e.clientY : e.touches[0].clientY);
+pos3 = e.type === 'mousemove' ? e.clientX : e.touches[0].clientX;
+pos4 = e.type === 'mousemove' ? e.clientY : e.touches[0].clientY;
+element.style.top = (element.offsetTop - pos2) + "px";
+element.style.left = (element.offsetLeft - pos1) + "px";
+socket.emit('position', { id: element.id, x: element.offsetLeft, y: element.offsetTop });
+}function closeDragElement() {document.removeEventListener('mouseup', closeDragElement);
+document.removeEventListener('mousemove', elementDrag);
+document.removeEventListener('touchend', closeDragElement);
+document.removeEventListener('touchmove', elementDrag);}}
 
 
-function addDraggableElement(id, x, y, team) {const newElement = document.createElement('img');newElement.id = id;newElement.classList.add(`draggable${team}`);
-
+// ----
+function addSoldierElement(id, x, y, team) {const newElement = document.createElement('img');newElement.id = id;newElement.classList.add(`draggable${team}`);
     newElement.src = "/Textures/Untitled.png"
-    
-newElement.style.left = x + 'px';newElement.style.top = y + 'px';document.body.appendChild(newElement);makeDraggable(newElement);newElement.addEventListener('dblclick', () =>{newElement.remove();socket.emit('removeElement', newElement.id);});let touchTimer;newElement.addEventListener('touchstart', function(event) {touchTimer = setTimeout(function() {newElement.remove();socket.emit('removeElement', newElement.id);console.log('Long press');}, 500);});newElement.addEventListener('touchmove', function(event) {clearTimeout(touchTimer);});
-newElement.addEventListener('touchend', function(event) {clearTimeout(touchTimer);});}
+    newElement.style.left = x + 'px';newElement.style.top = y + 'px';document.body.appendChild(newElement);makeDraggable(newElement);newElement.addEventListener('dblclick', () =>{newElement.remove();socket.emit('removeElement', newElement.id);});let touchTimer;newElement.addEventListener('touchstart', function(event) {touchTimer = setTimeout(function() {newElement.remove();socket.emit('removeElement', newElement.id);console.log('Long press');}, 500);});newElement.addEventListener('touchmove', function(event) {clearTimeout(touchTimer);});newElement.addEventListener('touchend', function(event) {clearTimeout(touchTimer);});
+};
+function addsoldier(Team) {const id = Date.now().toString(), x = 0, y = 0;
+    addSoldierElement(id, x, y, Team)
+    socket.emit('newElement', { id, x, y, Team });
+}
+// ----
 
-document.getElementById('addButton').addEventListener('click', function() {const id = Date.now().toString(), x = 0, y = 0, 
-team = 1;addDraggableElement(id, x, y, team);socket.emit('newElement', { id, x, y, team });});
+socket.on('newElement', function(data) {const existingElement = document.getElementById(data.id);if(existingElement){}else{
+addDraggableElement(data.id, data.x, data.y, data.team);}});
 
-document.getElementById('addButton1').addEventListener('click', function() {const id = Date.now().toString(), x = 0, y = 0, 
-team = 2;addDraggableElement(id, x, y, team);socket.emit('newElement', { id, x, y, team });});
-
-socket.on('newElement', function(data) {const existingElement = document.getElementById(data.id);
-if (existingElement) {}else{addDraggableElement(data.id, data.x, data.y, data.team);}});
-
-
-socket.on('position', function(data) {
-    const element = document.getElementById(data.id);
-    if (element) {
-        element.style.left = data.x + 'px';
-        element.style.top = data.y + 'px';
-    }
-});
-
-socket.on('removeElement', function(id) {
-    const elementToRemove = document.getElementById(id);
-    if (elementToRemove) {
-        elementToRemove.remove(); // Remove the corresponding element from other clients
-    }
-});
-
-
+socket.on('playersCount', function(data){document.querySelector(".playerCount").innerHTML = "Player Count "+data});
+socket.on('position', function(data) {const element = document.getElementById(data.id);
+if (element) {element.style.left = data.x + 'px';element.style.top = data.y + 'px';
+}});socket.on('removeElement', function(id) {const elementToRemove = document.getElementById(id);
+if (elementToRemove) {elementToRemove.remove();}});
 const fullscreenButton = document.getElementById('fullscreenButton');
 fullscreenButton.addEventListener('click', () => {toggleFullscreen();
 });function toggleFullscreen() {if (!document.fullscreenElement) {fullscreenButton.innerHTML = '<i class="fas fa-compress"></i>'
