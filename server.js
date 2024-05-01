145bce1b1e2e8de9426d39ebdda2f7ce0e1f06c4
Ars1290 <90035929+Ars12904578@users.noreply.github.com>
2024-05-01 06:05:14 +0000

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 443;
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
